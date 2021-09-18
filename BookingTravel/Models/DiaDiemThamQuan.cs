358fd09d64791b64b4af1f78488ac94cee88c4a4
= <=>
2021-09-18 10:41:26 +0700

    viet hoa tieu de bảng dia diem

--- BookingTravel/Models/DiaDiemThamQuan.cs
+++ BookingTravel/Models/DiaDiemThamQuan.cs
@@ -11,7 +11,7 @@ namespace BookingTravel.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
     public partial class DiaDiemThamQuan
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,10 +19,22 @@ namespace BookingTravel.Models
         {
             this.ChiTietDiaDiemThamQuan = new HashSet<ChiTietDiaDiemThamQuan>();
         }
-    
+
+        [Display(Name = "Mã địa danh")]
         public int ID { get; set; }
+
+        [Display(Name = "Tên địa danh")]
+        [Required(ErrorMessage = "Tên địa danh không được bỏ trống!")]
         public string TenDiaDanh { get; set; }
+
+
+        [Display(Name = "Tỉnh")]
+        [Required(ErrorMessage = "Chưa chọn tỉnh!")]
         public Nullable<short> Tinh { get; set; }
+
+
+        [Display(Name = "Khách sạn")]
+        [Required(ErrorMessage = "Chưa chọn kshách sạn!")]
         public Nullable<int> KhachSan_ID { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
