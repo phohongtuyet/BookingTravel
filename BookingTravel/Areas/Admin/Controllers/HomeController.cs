a6214fabbf64a12a88e8c8968d67aa545dd871b7
phtuyet <phohongtuyetse@gmail.com>
2021-09-23 19:03:41 +0700

    form login

--- BookingTravel/Areas/Admin/Controllers/HomeController.cs
+++ BookingTravel/Areas/Admin/Controllers/HomeController.cs
@@ -1,30 +1,61 @@
-﻿using System;
+﻿using BookingTravel.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.IO;
 using System.Web.Mvc;
+using BookingTravel.Libs;
 
-namespace BookingTravel.Controllers
+namespace BookingTravel.Areas.Admin.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		private BookingTravelEntities db = new BookingTravelEntities();
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+		public ActionResult Index()
+		{
+			return View();
+		}
+		public ActionResult Unauthorized()
+		{
+			return View();
+		}
+		// GET: Home/Login
+		public ActionResult Login()
+		{
+			ModelState.AddModelError("LoginError", "");
+			return View();
+		}
 
-            return View();
-        }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Login(NhanVienLogin nhanVien)
+		{
+			if (ModelState.IsValid)
+			{
+				string matKhauMaHoa = SHA1.ComputeHash(nhanVien.MatKhau);
+				var taiKhoan = db.NhanVien.Where(r => r.TenDangNhap == nhanVien.TenDangNhap && r.MatKhau == matKhauMaHoa).SingleOrDefault();
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+				if (taiKhoan == null)
+				{
+					ModelState.AddModelError("LoginError", "Tên đăng nhập hoặc mật khẩu không chính xác!");
+					return View(nhanVien);
+				}
+				else
+				{
+					// Đăng ký SESSION
+					Session["MaNhanVien"] = taiKhoan.ID;
+					Session["HoTenNhanVien"] = taiKhoan.HoVaTen;
+					Session["Quyen"] = taiKhoan.Quyen;
 
-            return View();
-        }
-    }
+					// Quay về trang chủ
+					return RedirectToAction("Index", "Home");
+				}
+			}
+
+			return View(nhanVien);
+		}
+
+	}
 }
--- BookingTravel/Controllers/HomeController.cs
+++ BookingTravel/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace BookingTravel.Areas.Admin.Controllers
+namespace BookingTravel.Controllers
 {
     public class HomeController : Controller
     {
