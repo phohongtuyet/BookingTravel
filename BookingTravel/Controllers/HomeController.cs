4cba6183fc6912cfffb9aed2a29c8ae4d91d9ad8
ctrtinh <ctrtinh07@gmail.com>
2021-10-04 11:18:22 +0700

    edit review bai viet

--- BookingTravel/Controllers/HomeController.cs
+++ BookingTravel/Controllers/HomeController.cs
@@ -1,30 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookingTravel.Models;
 
 namespace BookingTravel.Controllers
 {
     public class HomeController : Controller
     {
+        BookingTravelEntities db = new BookingTravelEntities();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Review()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var baiViet = db.BaiViet.Where(r => r.KiemDuyet == 1).ToList();
+            return View(baiViet);
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Search(FormCollection collection)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            string tukhoa = collection["Tukhoa"].ToString();
+            var baiViet = db.BaiViet.Where(r => r.KiemDuyet == 1 && (r.TieuDe.Contains(tukhoa) || r.NoiDung.Contains(tukhoa))).ToList();
+            return View(baiViet);
         }
+        public ActionResult New()
+        {
+            var baiViet = db.BaiViet.Where(r => r.KiemDuyet == 1).OrderByDescending(r => r.NgayDang).ToList();
+            return View(baiViet);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            // Lấy thông tin bài viết
+            var baiViet = db.BaiViet.Where(r => r.KiemDuyet == 1 && r.ID == id).SingleOrDefault();
+
+            // Tăng lượt xem
+            // Chính sách: Mỗi máy chỉ tăng 1 lượt xem
+            if (Object.Equals(Session["DaXem" + baiViet.ID], null))
+            {
+                BaiViet bv = db.BaiViet.Find(id);
+                bv.LuotXem = bv.LuotXem + 1;
+                db.Entry(bv).State = EntityState.Modified;
+                db.SaveChanges();
+
+                // Đánh dấu là đã xem
+                Session["DaXem" + bv.ID] = 1;
+            }
+
+            // Hiển thị bài viết ra View
+            return View(baiViet);
+        }
+
     }
 }
---
+++ BookingTravel/Libs/Images.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BookingTravel.Libs
+{
+    public class Images
+    {
+        public static string GetFirstImage(string input)
+        {
+            string matchString = Regex.Match(input, "<img.+?src=[\"'](.+?)[\"'].*?>", RegexOptions.IgnoreCase).Groups[1].Value;
+            if (!String.IsNullOrEmpty(matchString))
+                return matchString;
+            else
+                return "/Images/noimage.png";
+        }
+    }
+}
