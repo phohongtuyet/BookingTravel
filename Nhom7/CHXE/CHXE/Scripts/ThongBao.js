f3cf040e4f3e8a9c5b389033cbbdd8c9f999a6e8
caothanggg <caothang1332@gmail.com>
2021-10-05 20:28:07 +0700

    login va register

--- Nhom7/CHXE/CHXE/Scripts/ThongBao.js
+++
@@ -1,5 +0,0 @@
-﻿$(function () {
-    $('#AlertBox').removeClass('hide');
-    $('#AlertBox').delay(1500).slideUp(500);
-
-});
--- nhom16/readme.txt
+++
@@ -1,7 +0,0 @@
-Tài khoản khách hàng
-thang 123
-tinh 123
-
-Tài khoản nhân viên
-admin 123	
-user 123456
